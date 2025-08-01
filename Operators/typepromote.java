a59456750e0b5df1682674dbe3c6f7e73a60d68c
Nithin Dasari <nithindasari10@gmail.com>
2025-08-01 15:33:15 +0530

    Type propmotion

---
+++ Operators/typepromote.java
@@ -0,0 +1,21 @@
+public class typepromote {
+    public static void main(String[] args) {
+        byte a = 10;
+        char b='b';
+        byte c=12;
+        byte bt = (byte)(a+ b+c);//Gives the error as "possible lossy conversion from int to byte"
+                        // Even though there is no int type variable present 
+                        //It is happend because the compiler automatically converts 
+                        //byte,char,short to int when evaluating the expression
+        System.out.println(bt);
+
+
+        //and second type of promotion
+
+        int i=10;
+        float j=3.24f;
+        double k = 20.4;
+        double db = (i+j+k);
+        System.out.println(db);
+    }
+}
